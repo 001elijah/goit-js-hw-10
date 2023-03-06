d6c296495be2d903a19ec395667a8a8247bd2789
Illia Zolotukha <job.illia.igorevich@gmail.com>
2023-03-06 14:53:55 +0200

    update

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,14 +1,9 @@
 const BASE_URL = 'https://restcountries.com/v3.1';
-// const serchParams = new URLSearchParams({
-//     name: "name.official",
-//     capital: "capital",
-//     population: "population",
-//     flags: "flags.svg",
-//     languages: "languages"
-// });
-// console.log(serchParams.toString());
+const serchParams = new URLSearchParams({
+    fields: "name,capital,population,flags,languages"
+});
 export function fetchCountries(name) {
-    return fetch(`${BASE_URL}/name/${name}`)
+    return fetch(`${BASE_URL}/name/${name}?${serchParams}`)
     .then(response => {
         return response.json();
     });
